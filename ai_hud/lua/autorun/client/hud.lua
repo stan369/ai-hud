783ed407b21d4f7d7082640143377d7f536b541b
stan369 <redfishslayer2014@gmail.com>
2024-03-21 18:02:58 -0400

    Update hud.lua
    In this modified version, players can use the console command set_hud_scale to set the scale of the HUD. For example, set_hud_scale 1.5 This will increase the size of the HUD by 1.5 times its original size.

--- ai_hud/lua/autorun/client/hud.lua
+++ ai_hud/lua/autorun/client/hud.lua
@@ -1,17 +1,20 @@
 
+-- Default scale values
+local defaultScaleWidth = 1
+local defaultScaleHeight = 1
 
--- Hide the default Garry's Mod HUD
-local function HideDefaultHUD(name)
-    if name == "CHudHealth" or name == "CHudBattery" then
-        return false
-    end
+-- Function to update the scale
+local function UpdateScale(width, height)
+    defaultScaleWidth = width
+    defaultScaleHeight = height
 end
-hook.Add("HUDShouldDraw", "HideDefaultHUD", HideDefaultHUD)
 
-
-
--- Everything below is AI Generated.
-
+-- Command to set the scale
+concommand.Add("set_hud_scale", function(ply, _, args)
+    local width = tonumber(args[1]) or 1
+    local height = tonumber(args[2]) or 1
+    UpdateScale(width, height)
+end)
 
 -- Hook into the HUDPaint event which is called every frame
 hook.Add("HUDPaint", "DrawMyHud", function()
@@ -24,16 +27,19 @@ hook.Add("HUDPaint", "DrawMyHud", function()
     local health = ply:Health() or 0
     local armor = ply:Armor() or 0
 
+    -- Calculate scaling factor
+    local scaleWidth, scaleHeight = defaultScaleWidth, defaultScaleHeight
+
     -- Set the position and size for the HUD elements
-    local posX, posY = 50, ScrH() - 50 -- Position from the bottom left corner
-    local sizeX, sizeY = 200, 40 -- Size of the health and armor bars
+    local posX, posY = 50 * scaleWidth, (ScrH() - 50) * scaleHeight -- Position from the bottom left corner
+    local sizeX, sizeY = 200 * scaleWidth, 40 * scaleHeight -- Size of the health and armor bars
 
     -- Draw the background for the health bar
-    draw.RoundedBox(4, posX, posY - sizeY - 10, sizeX, sizeY, Color(40, 40, 40, 200))
+    draw.RoundedBox(4, posX, posY - sizeY - 10 * scaleHeight, sizeX, sizeY, Color(40, 40, 40, 200))
 
     -- Draw the health bar
     local healthWidth = (math.Clamp(health, 0, 100) / 100) * (sizeX - 2)
-    draw.RoundedBox(4, posX + 1, posY - sizeY - 9, healthWidth, sizeY - 2, Color(255, 0, 0, 200))
+    draw.RoundedBox(4, posX + 1, posY - sizeY - 9 * scaleHeight, healthWidth, sizeY - 2, Color(255, 0, 0, 200))
 
     -- Draw the text for the health
     draw.SimpleText("Health: " .. health, "Trebuchet24", posX + 5, posY - sizeY, Color(255, 255, 255, 255), TEXT_ALIGN_LEFT, TEXT_ALIGN_TOP)
@@ -49,3 +55,10 @@ hook.Add("HUDPaint", "DrawMyHud", function()
     draw.SimpleText("Armor: " .. armor, "Trebuchet24", posX + 5, posY + 5, Color(255, 255, 255, 255), TEXT_ALIGN_LEFT, TEXT_ALIGN_TOP)
 end)
 
+-- Hide the default Garry's Mod HUD
+local function HideDefaultHUD(name)
+    if name == "CHudHealth" or name == "CHudBattery" then
+        return false
+    end
+end
+hook.Add("HUDShouldDraw", "HideDefaultHUD", HideDefaultHUD)
